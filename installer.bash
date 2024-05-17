8be8bddb4c74da3128c6871599645163e4ca98bf
B4K35 <107258769+B4K35@users.noreply.github.com>
2024-05-17 19:00:04 +1000

    Update installer.bash

--- installer.bash
+++ installer.bash
@@ -3,6 +3,7 @@
 # Define an array of programs to install
 PROGRAMS=(
   "python3"
+  "unzip"
   "grep" # Useful for most tasks
   "ngrep" # Useful for most tasks
   "nmap"
@@ -11,11 +12,13 @@ PROGRAMS=(
   "tshark" # Task 3P & 5.1P
   "tcpdump" # Task C1
   "aircrack-ng" # Task D2
-  "bison" # Task 2.1 & D1
-  "flex" # Task 2.1 & D1
-  "rrdtool" # Task 2.1 & D1
-  "librrd-dev argus-client" # Task 2.1 & D1
-  "./imhex-*.deb" # Task 1C
+  # The next set of programs are installed with NFdump but will need
+  # to be installed by the user if the repository wasn't cloned.
+  # "bison" # Task 2.1 & D1
+  # "flex" # Task 2.1 & D1
+  # "rrdtool" # Task 2.1 & D1
+  # "librrd-dev" # Task 2.1 & D1
+  # "argus-client" # Task 2.1 & D1
 )
 
 # Update the package list
@@ -40,18 +43,32 @@ done
 # Install older nfdump version (Using clang-10 in our array would work if we wanted up to date)
 wget http://sourceforge.net/projects/nfdump/files/nsel/nfdump-1.5.8-NSEL/nfdump-1.5.8-NSEL.tar.gz/download
 tar xzf download
-cd nfdump-1.5.8-NSEL/
 
 # Copy downloaded files (assuming they are in the current directory)
-rm nf_common.c
-rm nf_common.h .
-
-./configure --enable-nfprofile
-make
-make install
-echo "Nfdump successfully installed and is ready for .c & .h files to be added."
-cd ..
-
+if [ -d "SIT327 Task Resources" ]; then
+  echo "Task Resources present preparing for first task"
+  echo "Unzipping Task Resources ..."
+  cd "SIT327 Task Resources/"
+  for file in *.zip; do
+    filename="${file%.*}"
+    makdir -p "$filename"
+    echo y | unzip -q "$file" -d "$filename"
+  done
+  cd ..
+  cp -f "SIT327 Task Resources/2.1P-resources/nf_common.c" "nfdump-1.5.8-NSEL/"
+  cp -f "SIT327 Task Resources/2.1P-resources/nf_common.h" "nfdump-1.5.8-NSEL/"
+  cp "SIT327 Task Resources/2.1P-resources/argus-collector.ra" "nfdump-1.5.8-NSEL/"
+  echo "\nReady to complete task 2.1P"
+  sudo apt-get install bison flex rrdtool librrd-dev argus-client
+  cd nfdump-1.5.8-NSEL/
+  ./configure --enable-nfprofile
+  make
+  make install
+  cd ..
+  echo "\n Great success!"
+else 
+  echo "Task Resources not present you will need to configure NFdump!"
+fi
 
 # Clean up unnecessary packages and files
 echo "Cleaning up..."
@@ -69,5 +86,5 @@ for PROGRAM in "${PROGRAMS[@]}"; do
     echo "Error: $PROGRAM is not installed correctly."
   fi
 done
-
+echo "Good luck this Trimester!"
 echo "Script execution completed."
